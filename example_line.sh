c1304b787f9af6d4eeecaeaf679e0b5fb02df48f
luke reding <lukereding@gmail.com>
2015-10-08 11:43:06 -0500

    Update example_line.sh

--- example_line.sh
+++ example_line.sh
@@ -1,1 +1,8 @@
+# brew install ucto
+# also handy to convert pdf docs to plain text:
+# brew install poppler
+# then
+# pdftotext input.pdf output.txt
+
+# example
 ucto -L en -n -s ' ' ~/Documents/behavior_and_technology/*.md | readability
