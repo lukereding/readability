dcedd5e3bd9a17c543b383c2f203d0395d4775b4
luke reding <lukereding@gmail.com>
2015-10-08 17:14:14 -0500

    Update print_reading_stats.sh

--- print_reading_stats.sh
+++ print_reading_stats.sh
@@ -9,3 +9,4 @@ do
   KINCAID= $(ucto -L en -n -s ' ' ~/Documents/behavior_and_technology/$NAME.txt | readability | grep 'Kincaid' | grep -o '[0-9].*')
   AVG_SYL= $(ucto -L en -n -s ' ' ~/Documents/behavior_and_technology/$NAME.txt | readability | grep 'syll_per_word' | grep -o '[0-9].*')
   WORDS_PER_SENTENCE= $(ucto -L en -n -s ' ' ~/Documents/behavior_and_technology/$NAME.txt | readability | grep 'words_per_sentence' | grep -o '[0-9].*')
+  echo $KINCAID, $AVG_SYL, $WORDS_PER_SENTENCE
